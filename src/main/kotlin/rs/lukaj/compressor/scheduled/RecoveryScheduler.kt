48ab3c5c22d94c5adb677a1e4bec89b041a8e907
Luka Jovicic <luka.jovicic16+git@gmail.com>
2020-12-02 01:54:17 +0100

    Tweaks for recovery scheduler: don't fail on exceptions

--- src/main/kotlin/rs/lukaj/compressor/scheduled/RecoveryScheduler.kt
+++ src/main/kotlin/rs/lukaj/compressor/scheduled/RecoveryScheduler.kt
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import rs.lukaj.compressor.configuration.EnvironmentProperties
+import rs.lukaj.compressor.model.Video
 import rs.lukaj.compressor.model.VideoStatus
 import rs.lukaj.compressor.service.FileService
 import rs.lukaj.compressor.service.VideoService
@@ -29,14 +30,25 @@ class RecoveryScheduler(
         //this assumes filenames are video IDs (UUIDs) and as such is very brittle
         val filesForRecovery = files.getRecoveryVideos()
         for(file in filesForRecovery) {
-            val video = service.getVideo(UUID.fromString(file.name))
-            if(video.isEmpty || video.get().status != VideoStatus.ERROR) {
-                logger.warn { "Invalid video $video: won't attempt recovery" }
+            val video : Video
+            try {
+                val optVideo = service.getVideo(UUID.fromString(file.name))
+                if(optVideo.isEmpty || optVideo.get().status != VideoStatus.ERROR) {
+                    logger.warn { "Invalid video $optVideo: won't attempt recovery" }
+                    continue
+                }
+                video = optVideo.get()
+            } catch (e: Exception) {
+                logger.error(e) { "Error while parsing video data for recovery. Skipping video ${file.name}." }
                 continue
             }
+
+            logger.info { "Recovering video ${video.id} (${video.name})" }
             val fis = BufferedInputStream(FileInputStream(file))
             try {
-                service.addToQueue(fis, video.get().name, video.get().originalSize, video.get().email)
+                val queueFile = files.getQueueVideo(video.id!!)
+                queueFile.delete() //we don't care if this fails - queue file might really not be there
+                service.addToQueue(fis, video.name, file.length(), video.email)
                 file.delete()
             } catch (e: Exception) {
                 logger.warn(e) { "Exception occurred while attempting to add video for recovery" }
